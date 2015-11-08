189a3fde9cba69ae0768c24012bc8ec64b483606
Takehiro Mouri <takehiromouri@gmail.com>
2015-11-08 20:57:08 +0000

    Setup STI

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module TeamEndgame
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # autoload pieces models
+    config.autoload_paths += %W(#{config.root}/app/models/pieces)
   end
 end
