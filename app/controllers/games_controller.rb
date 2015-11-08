8e5795c2d991e295eb3bc04c0c34979aa8ec122f
Takehiro Mouri <takehiromouri@gmail.com>
2015-11-08 17:24:54 +0000

    Fixed tabbing

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,38 +10,37 @@ class GamesController < ApplicationController
   end
 
   def create
-	Game.create(game_params)
-	redirect_to root_path
+  Game.create(game_params)
+  redirect_to root_path
   end
 
   def show
-	@game = Game.find(params[:id])
+  @game = Game.find(params[:id])
   end
 
   def edit
-	@game = Game.find(params[:id])
+  @game = Game.find(params[:id])
   end
 
   def update
-	@game = Game.find(params[:id])
-	if @game.update(game_params)
-	  redirect_to root_path
-	else
-	  render 'edit'
-	end
+    @game = Game.find(params[:id])
+    if @game.update(game_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-	@game = Game.find(params[:id])
-	@game.destroy
-	flash[:notice] = "Game Deleted"
-	redirect_to root_path
+    @game = Game.find(params[:id])
+    @game.destroy
+    flash[:notice] = "Game Deleted"
+    redirect_to root_path
   end
 
   private
 
   def game_params
-	params.require(:game).permit(:name, :white_player_id, :black_player_id)
+    params.require(:game).permit(:name, :white_player_id, :black_player_id)
   end
-  
 end
