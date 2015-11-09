8e40db46d56fb93afade28df8f50c4d0a131867d
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2015-11-09 00:20:40 +0000

    Added more tests for game model

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,10 +1,33 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
+  def setup
+    @g = Game.create(name: "New Game", white_player_id: 1, black_player_id: 1)
+  end
+
   test "board populated with pieces" do
-    g = Game.create(name: "New Game", white_player_id: 1, black_player_id: 1)
     expected = 32
-    actual = g.pieces.count
+    actual = @g.pieces.count
     assert_equal expected, actual
+  end
+
+  test "king in correct position" do
+    expected = [4, 7]
+    actual = [@g.pieces.last.col_position, @g.pieces.last.row_position]
+    assert_equal expected, actual
+
+    expected_type = "King"
+    actual_type = @g.pieces.last.type
+    assert_equal expected_type, actual_type
+  end
+
+  test "pawn in correct position" do
+    expected = [7, 1]
+    actual = [@g.pieces.first.col_position, @g.pieces.first.row_position]
+    assert_equal expected, actual
+
+    expected_type = "Pawn"
+    actual_type = @g.pieces.first.type
+    assert_equal expected_type, actual_type
   end
 end
