c8dccf56b99fc4dbdfd687ae0293969c7c10a307
Adam D <addstar34@gmail.com>
2015-11-18 01:08:02 +0000

    def is_obstructed? in piece.rb

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,9 @@
 class Piece < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
+
+  def is_obstructed?
+
+  end
+  
 end
--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class PieceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	
+	
 end
