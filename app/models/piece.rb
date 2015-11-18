8fd0d110ec3682799bf8226ced3240451ea1ed6f
Adam D <addstar34@gmail.com>
2015-11-18 06:33:44 +0000

    updates to is_obstructed? method in piece.rb

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,8 +2,43 @@ class Piece < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
 
-  def is_obstructed?
+  def is_obstructed?(row_dest, col_dest)
+  	# pass in row and col destination
+  	# get the current piece position
+  	# work out if the move is horizontal or diagonal or vertical
+  	# if is check obstruction
 
+
+  	row_pos = self.row_position
+  	col_pos = self.col_position
+  	current_user = self.user_id
+
+
+
+  	if col_pos == col_dest # this means move is vertical
+
+
+  		# return self.game.pieces.where(row_position: 6, col_position: 7).first
+
+
+  		return "vertical move"
+  	elsif row_pos == row_dest
+
+
+
+  		return self.game.pieces.where(row_position: 7, col_position: 5)
+
+
+  		# return "horizontal move"
+  	elsif (col_pos - col_dest).abs == (row_pos - row_dest).abs
+  		return "diagonal move"
+  	else
+  		return "else"
+  	end
+
+
+
+  	return "Bla bla bla"
   end
   
 end
--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class PieceTest < ActiveSupport::TestCase
+	def create_game
+		@user1 = FactoryGirl.create(:user)
+	  @user2 = FactoryGirl.create(:user)
+		@g = Game.create(name: "New Game", white_player_id: @user1.id, black_player_id: @user2.id)
+	end
 
-	
-	
+	test "is_obstructed?" do
+		create_game
+
+		# the last Rook that gets created on the board is a black rook in row 7 col 7.
+		@rook1 = Rook.last
+		@piece = Piece.where(row_position: 6, col_position: 3).first
+			
+		# expecting an Obstruction as Rook is obstructed at the start of the game.
+		expected = "OBSTRUCTION for ROOK no moves possible"
+		# actual = @piece.is_obstructed?(0, 4)
+	  actual = @rook1.is_obstructed?(7, 5)
+	  assert_equal expected, actual
+
+	end
+
 end
