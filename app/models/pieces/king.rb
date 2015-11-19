490bbd0cd53d6ebc0af1bf1463b89ac2e91651b1
Ronny Almog <ronny2205@gmail.com>
2015-11-19 16:38:30 +0000

    removing unnecessary variables

--- app/models/pieces/king.rb
+++ app/models/pieces/king.rb
@@ -1,17 +1,14 @@
 class King < Piece
   # rubocop:disable Metrics/LineLength
   def valid_move?(row_dest, col_dest)
-    row_pos = row_position
-    col_pos = col_position
-    current_user = user_id
-    row_diff = (row_pos - row_dest).abs
-    col_diff = (col_pos - col_dest).abs
+    row_diff = (row_position - row_dest).abs
+    col_diff = (col_position - col_dest).abs
     # checking weather the move is longer than one step to any direction
     if row_diff > 1 || col_diff > 1
       return false
     else
       # king is inside destination. checking if a piece from the same color is there.
-      return game.pieces.find_by(row_position: row_dest, col_position: col_dest, user_id: current_user).nil?
+      return game.pieces.find_by(row_position: row_dest, col_position: col_dest, user_id: user_id).nil?
     end
   end
 end
