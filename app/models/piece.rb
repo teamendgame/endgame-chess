5f00d074e2ec2ea4a4713ab28ac624ad6dec7807
Adam D <addstar34@gmail.com>
2015-11-24 03:43:55 +0000

    obstructed? method updated

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,89 +2,74 @@ class Piece < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
 
-  def is_obstructed?(row_dest, col_dest)
-  	# pass in row and col destination
-  	# get the current piece position
-  	# work out if the move is horizontal or diagonal or vertical
-  	# if is check obstruction
+  def obstructed?(row_dest, col_dest)
+    # pass in row and col destination
+    # get the current piece position
+    # work out if the move is horizontal or diagonal or vertical
+    # if is check obstruction
     # this method is not checking destinations outside of the board
     # because the players should not be able to select a destination outside of the board
     # if knight return an error
 
-  	row_pos = self.row_position
-  	col_pos = self.col_position
-  	current_user = self.user_id
+    row_pos = row_position
+    col_pos = col_position
 
-    if self.type == "Knight"
+    if type == "Knight"
       return "Invalid input! Knight can't be obstructed."
     end
 
-  	if col_pos == col_dest # this is checking vertical obstruction
+    if col_pos == col_dest # this is checking vertical obstruction
       if row_pos < row_dest
         # find out which is higher to create a range and return pieces within that range
         row_pos += 1
-        return !self.game.pieces.where(captured: false, row_position: [row_pos...row_dest], col_position: col_pos).first.nil?
+        return !game.pieces.find_by(captured: false, row_position: [row_pos...row_dest], col_position: col_pos).nil?
       else
         row_dest += 1
-        return !self.game.pieces.where(captured: false, row_position: [row_dest...row_pos], col_position: col_pos).first.nil?
+        return !game.pieces.find_by(captured: false, row_position: [row_dest...row_pos], col_position: col_pos).nil?
       end
-  		return "vertical move"
-  	elsif row_pos == row_dest # this is checking horizontal obstruction
+    elsif row_pos == row_dest # this is checking horizontal obstruction
       if col_pos < col_dest
-        col_pos += 1 
+        col_pos += 1
         col_dest -= 1
-        return !self.game.pieces.where(row_position: row_pos, col_position: [col_pos..col_dest]).first.nil?
+        return !game.pieces.find_by(captured: false, row_position: row_pos, col_position: [col_pos..col_dest]).nil?
       else
         col_pos -= 1
         col_dest += 1
-        return !self.game.pieces.where(row_position: row_pos, col_position: [col_dest..col_pos]).first.nil?
+        return !game.pieces.find_by(captured: false, row_position: row_pos, col_position: [col_dest..col_pos]).nil?
       end
-  	elsif (col_pos - col_dest).abs == (row_pos - row_dest).abs # is checking diagonal obstruction
-      # check each row/col combo 1 by 1     
+    elsif (col_pos - col_dest).abs == (row_pos - row_dest).abs # is checking diagonal obstruction
+      # check each row/col combo 1 by 1
       # row and col both increase
       if row_pos < row_dest && col_pos < col_dest
         row_pos += 1
         (row_pos...row_dest).each do |row_num|
           col_pos += 1
-          if !self.game.pieces.where(row_position: row_num, col_position: col_pos).first.nil?
-            return true
-          end
+          return !game.pieces.find_by(captured: false, row_position: row_num, col_position: col_pos).nil?
         end
-        return false
       # row and col both decrease
       elsif row_pos > row_dest && col_pos > col_dest
         row_dest += 1
         (row_dest...row_pos).each do |row_num|
           col_dest += 1
-          if !self.game.pieces.where(row_position: row_num, col_position: col_dest).first.nil?
-            return true
-          end
+          return !game.pieces.find_by(captured: false, row_position: row_num, col_position: col_dest).nil?
         end
-        return false
       # row increases col decreases
       elsif row_pos < row_dest && col_pos > col_dest
         row_pos += 1
         (row_pos...row_dest).each do |row_num|
           col_pos -= 1
-          if !self.game.pieces.where(row_position: row_num, col_position: col_pos).first.nil?
-            return true
-          end
+          return !game.pieces.find_by(captured: false, row_position: row_num, col_position: col_pos).nil?
         end
-        return false
       # row decreases col increases
       else
         row_dest += 1
         (row_dest...row_pos).each do |row_num|
           col_dest -= 1
-          if !self.game.pieces.where(row_position: row_num, col_position: col_dest).first.nil?
-            return true
-          end
+          return !game.pieces.where(captured: false, row_position: row_num, col_position: col_dest).nil?
         end
-        return false
       end
-  	else
-  		return "ERROR! in is_obstructed? method line:83"
-  	end
+    else
+      return "ERROR! in is_obstructed? method line:83"
+    end
   end
-  
 end
--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -1,38 +1,47 @@
 require 'test_helper'
 
 class PieceTest < ActiveSupport::TestCase
-	def create_game
-		@user1 = FactoryGirl.create(:user)
-	  @user2 = FactoryGirl.create(:user)
-		@g = Game.create(name: "New Game", white_player_id: @user1.id, black_player_id: @user2.id)
-	end
+  def create_game
+    @user1 = FactoryGirl.create(:user)
+    @user2 = FactoryGirl.create(:user)
+    @g = Game.create(name: "New Game", white_player_id: @user1.id, black_player_id: @user2.id)
+  end
 
-	test "is_obstructed?" do
-		create_game
+  test "obstructed?" do
+    create_game
 
-		# test moving to a destination where there is a piece
-		# get the black pawn, set it to captured, test rook can move to white pawn in same column
-		@pawn_black = Piece.where(row_position: 6, col_position: 0).first
-		@pawn_black.update(captured: true)
-		@rook_black = Piece.where(row_position: 7, col_position: 0).first
+    # test moving to a destination where there is a piece but no obstruction
+    # get the black pawn, set it to captured, test rook can move to white pawn in same column
+    @pawn_black = Piece.where(row_position: 6, col_position: 0).first
+    @pawn_black.update(captured: true)
+    @rook_black = Piece.where(row_position: 7, col_position: 0).first
 
-		expected = false
-	  actual = @rook_black.is_obstructed?(1, 0)
-	  assert_equal expected, actual
+    expected = false
+    actual = @rook_black.obstructed?(1, 0)
+    assert_equal expected, actual
 
-	  # test is_obstructed? for a knight
-	  @knight_white = Piece.where(row_position: 0, col_position: 1).first
+    # test rook_black for a horizontal move where there is an obstruction
+    expected = true
+    actual = @rook_black.obstructed?(7, 2)
+    assert_equal expected, actual
 
-	  expected = "Invalid input! Knight can't be obstructed."
-	  actual = @knight_white.is_obstructed?(2, 2)
-	  assert_equal expected, actual
+    # test is_obstructed? for a knight
+    @knight_white = Piece.where(row_position: 0, col_position: 1).first
 
-	  # test for obstruction where move is diagonal
-	  @bishop_white = Piece.where(row_position: 0, col_position: 2).first
+    expected = "Invalid input! Knight can't be obstructed."
+    actual = @knight_white.obstructed?(2, 2)
+    assert_equal expected, actual
 
-	  expected = true
-	  actual = @bishop_white.is_obstructed?(2, 4)
-	  assert_equal expected, actual
-	end
+    # test for obstruction where move is diagonal
+    @bishop_white = Piece.where(row_position: 0, col_position: 2).first
+    @pawn_white = Piece.where(row_position: 1, col_position: 3).first
+    # @pawn_white.update(captured: true)
 
+    expected = true
+    # actual = Piece.find_by(row_position: 1, col_position: 2).nil?
+    # actual = Piece.where(row_position: 0, col_position: 2).nil?
+    # actual = @pawn_white
+    actual = @bishop_white.obstructed?(2, 4)
+    assert_equal expected, actual
+  end
 end
