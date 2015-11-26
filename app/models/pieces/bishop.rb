fffaa01e0af1bc47d3e99294cad573577146b33d
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2015-11-26 10:03:29 +0000

    Add valid move for bishop and include tests

--- app/models/pieces/bishop.rb
+++ app/models/pieces/bishop.rb
@@ -1,2 +1,8 @@
 class Bishop < Piece
+  def valid_move?(row_dest, col_dest)
+    # Bishop can only move diagonally
+    # rubocop:disable Metrics/LineLength
+    return false if !self.diagonal_move?(row_dest, col_dest) || !self.own_piece?(row_dest, col_dest)
+    true
+  end
 end
