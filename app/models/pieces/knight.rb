b3f016b5acd57a9517b0f41eede516e82b75eb28
Takehiro Mouri <takehiromouri@gmail.com>
2015-11-26 21:10:51 +0000

    Fixed King valid_move

--- app/models/pieces/knight.rb
+++ app/models/pieces/knight.rb
@@ -2,7 +2,7 @@ class Knight < Piece
   def valid_move?(row_dest, col_dest)
     vertical = (row_position - row_dest).abs
     horizontal = (col_position - col_dest).abs
-    return false if !self.own_piece?(row_dest, col_dest) || (vertical + horizontal) != 3
+    return false if self.own_piece?(row_dest, col_dest) || (vertical + horizontal) != 3
     true
   end
 end
