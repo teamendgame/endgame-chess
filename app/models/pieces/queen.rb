75fe33c6e9a371a3796e69b47a61825e837ed400
Ronny Almog <ronny2205@gmail.com>
2015-11-27 19:14:03 +0000

    valid move for queen + tests

--- app/models/pieces/queen.rb
+++ app/models/pieces/queen.rb
@@ -1,2 +1,17 @@
 class Queen < Piece
+  # rubocop:disable Metrics/LineLength	
+  def valid_move?(row_dest, col_dest)
+    # checking whether the move is horizontal, diagonal or vertical
+    if !horizontal_move?(row_dest, col_dest) && !vertical_move?(row_dest, col_dest) && !diagonal_move?(row_dest, col_dest)
+    	puts "hh"
+      return false
+    # checking for obstruction  
+    elsif obstructed?(row_dest, col_dest)
+      return false
+    # checking if a piece from the same color is there
+    else
+      return !self.own_piece?(row_dest, col_dest)
+    end  
+  end
 end
+   
---
+++ test/models/queen_test.rb
@@ -0,0 +1,86 @@
+require 'test_helper'
+class QueenTest < ActiveSupport::TestCase
+  def setup
+    @user1 = FactoryGirl.create(:user)
+    @user2 = FactoryGirl.create(:user)
+    @g = Game.create(name: "New Game", white_player_id: @user1.id, black_player_id: @user2.id)
+    @g.populate_board!
+  end
+
+  # test "queen steps on own king" do
+  #   # the last Queen that gets created on the board is a black king at position (7,3)
+  #   @queen1 = Queen.last
+  #   expected = false
+  #   actual = @queen1.valid_move?(7, 2)
+  #   assert_equal expected, actual
+  # end
+
+
+  # test "queen obstructed" do
+  #   # the last Queen that gets created on the board is a black king at position (7,3)
+  #   @queen1 = Queen.last
+  #   expected = false
+  #   actual = @queen1.valid_move?(5, 1)
+  #   assert_equal expected, actual
+  # end
+  # test "inside destination but blocked" do
+  #   # the last King that gets created on the board is a black king at position (7,4)
+  #   @king1 = King.last
+  #   expected = false
+  #   actual = @king1.valid_move?(7, 3)
+  #   assert_equal expected, actual
+  # end
+
+  # test "inside destination, not blocked" do
+  #   # creating an extra white king in a location where it can move
+  #   # rubocop:disable Metrics/LineLength
+  #   @king1 = Piece.create(type: "King", row_position: 5, col_position: 4, user_id: @user1.id, game_id: @g.id)
+  #   expected = true
+  #   actual = @king1.valid_move?(6, 4)
+  #   assert_equal expected, actual
+  # end
+
+  # test "valid horizontal move" do
+  #   # creating an extra black quuen in a location where it can move
+  #   #setup
+  #   @queen1 = Piece.create(type: "Queen", row_position: 5, col_position: 4, user_id: @user2.id, game_id: @g.id)
+  #   expected = true
+  #   actual = @queen1.valid_move?(5, 1)
+  #   assert_equal expected, actual
+  # end
+
+  # test "valid vertical move" do
+  #   # creating an extra black quuen in a location where it can move
+  #   #setup
+  #   @queen1 = Piece.create(type: "Queen", row_position: 5, col_position: 4, user_id: @user2.id, game_id: @g.id)
+  #   expected = true
+  #   actual = @queen1.valid_move?(2, 4)
+  #   assert_equal expected, actual
+  # end
+
+  # test "valid diagonal move" do
+  #   # creating an extra black quuen in a location where it can move
+  #   @queen1 = Piece.create(type: "Queen", row_position: 5, col_position: 4, user_id: @user2.id, game_id: @g.id)
+  #   expected = true
+  #   actual = @queen1.valid_move?(3, 2)
+  #   assert_equal expected, actual
+  # end 
+
+  test "valid diagonal move2" do
+    # creating an extra black quuen in a location where it can move
+    setup
+    @queen1 = Piece.create(type: "Queen", row_position: 4, col_position: 4, user_id: @user2.id, game_id: @g.id)
+    expected = true
+    actual = @queen1.valid_move?(2, 6)
+    assert_equal expected, actual
+  end 
+
+  # test "wrong direction move" do
+  #   # creating an extra black quuen in a location where it can move
+  #   #setup
+  #   @queen1 = Piece.create(type: "Queen", row_position: 5, col_position: 4, user_id: @user2.id, game_id: @g.id)
+  #   expected = false
+  #   actual = @queen1.valid_move?(2, 0)
+  #   assert_equal expected, actual
+  # end 
+end
