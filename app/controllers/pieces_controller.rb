aa338268d9888e21b25e01460a3fcee288c9dab3
Takehiro Mouri <takehiromouri@gmail.com>
2015-11-27 05:06:40 +0000

    Implemented castling to UI

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -11,4 +11,30 @@ class PiecesController < ApplicationController
     # rubocop:disable Metrics/LineLength
     redirect_to game_path(@piece.game_id) if @piece.update_attributes(row_position: params[:row_position], col_position: params[:col_position]) && @game.update_attributes(turn_number: @game.turn_number + 1)
   end
+
+  def castle_kingside
+    @piece = Piece.find(params[:piece_id])
+    @game = Game.find(@piece.game_id)
+      if @game.black_player_id == current_user.id
+        king = King.find_by(user_id: current_user.id)
+        flash[:alert] = "You cannot castle" unless king.can_castle?(7, 7)
+      elsif @game.white_player_id == current_user.id
+        king = King.find_by(user_id: current_user.id)
+        flash[:alert] = "You cannot castle" unless king.can_castle?(0, 7)
+      end
+    redirect_to game_path(@piece.game_id)
+  end
+
+  def castle_queenside
+    @piece = Piece.find(params[:piece_id])
+    @game = Game.find(@piece.game_id)
+      if @game.black_player_id == current_user.id
+        king = King.find_by(user_id: current_user.id)
+        flash[:alert] = "You cannot castle" unless king.can_castle?(7, 0)
+      elsif @game.white_player_id == current_user.id
+        king = King.find_by(user_id: current_user.id)
+        flash[:alert] = "You cannot castle" unless king.can_castle?(0, 0)
+      end
+      redirect_to game_path(@piece.game_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks", registrations: 'registrations' }
   root 'homepage#index'
   resources :games
-  resources :pieces
+  resources :pieces do
+    get :castle_kingside, to: 'pieces#castle_kingside'
+    get :castle_queenside, to: 'pieces#castle_queenside'
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
