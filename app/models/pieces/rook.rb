bbae338c9a0970f202888dfdafe5c51c04e17ace
Adam D <addstar34@gmail.com>
2015-12-01 03:57:21 +0000

    valid move for rook and tests completed

--- app/models/pieces/rook.rb
+++ app/models/pieces/rook.rb
@@ -1,31 +1,11 @@
 class Rook < Piece
   def valid_move?(row_dest, col_dest)
     # Rook can only move horizontally and vertically
-    if self.horizontal_move?(row_dest, col_dest) || self.vertical_move?(row_dest, col_dest)
-      # return !own_piece?(row_dest, col_dest)
-      # return !obstructed?(row_dest, col_dest)
-      if !self.obstructed?(row_dest, col_dest) #&& !own_piece?(row_dest, col_dest) # own_piece? method should only checked uncaptured pieces!?
-        "can't get here for some reason"
-      else
-        "and can't get here EITHER!?"
-      end
-      return "what is this scenario!?"
+    # rubocop:disable Metrics/LineLength
+    if horizontal_move?(row_dest, col_dest) || vertical_move?(row_dest, col_dest) && !obstructed?(row_dest, col_dest) && !own_piece?(row_dest, col_dest)
+      return true
     else
-      false
+      return false
     end
   end
-
-
-
-  # def valid_move?(row_dest, col_dest)
-  #   # Rook can only move horizontally and vertically
-  #   if self.horizontal_move?(row_dest, col_dest) || self.vertical_move?(row_dest, col_dest) && !self.obstructed?(row_dest, col_dest) && !self.own_piece?(row_dest, col_dest) # own_piece? method should only checked uncaptured pieces!?
-  #     return true
-  #   else
-  #     return false
-  #   end
-  # end
-
-
-
 end
--- test/models/rook_test.rb
+++ test/models/rook_test.rb
@@ -5,23 +5,52 @@ class RookTest < ActiveSupport::TestCase
     @user2 = FactoryGirl.create(:user)
     @g = Game.create(name: "New Game", white_player_id: @user1.id, black_player_id: @user2.id)
     @g.populate_board!
-    @black_bishop = @g.pieces.where(row_position: 7, col_position: 5, type: "Bishop").first
+    @rook_black = Piece.find_by(row_position: 7, col_position: 7)
+    @pawn_black = Piece.find_by(row_position: 6, col_position: 7)
+    @pawn_black.update(captured: true)
   end
 
-  test "valid move for rook" do
-  	@rook_black = Piece.find_by(row_position: 7, col_position: 7)
-  	@pawn_black = Piece.find_by(row_position: 6, col_position: 7)
-  	@pawn_black.update(captured: true)
-  	@row_dest = 6
-  	@col_dest = 7
+  # valid moves
 
-		expected = "bla bla"
-		actual = @rook_black.valid_move?(0, 7)
-		assert_equal expected, actual
- 		
+  test "rook valid move vertical" do
+    expected = true
+    actual = @rook_black.valid_move?(3, 7)
+    assert_equal expected, actual
   end
 
+  test "rook valid move vertical lands on opponent" do
+    expected = true
+    actual = @rook_black.valid_move?(1, 7)
+    assert_equal expected, actual
+  end
+
+  test "rook valid move horizontal" do
+    @knight_black = Piece.find_by(row_position: 7, col_position: 6)
+    @knight_black.update(captured: true)
+    expected = true
+    actual = @rook_black.valid_move?(7, 6)
+    assert_equal expected, actual
+  end
+
+  # invalid moves
+
+  test "rook invalid move vertical obstructed opponent piece" do
+    expected = false
+    actual = @rook_black.valid_move?(0, 7)
+    assert_equal expected, actual
+  end
+
+  test "rook invalid move vertical obstruction own piece" do
+    @pawn_black.update(captured: false)
+    expected = false
+    actual = @rook_black.valid_move?(5, 7)
+    assert_equal expected, actual
+  end
+
+  test "rook invalid move vertical own piece in destination" do
+    @pawn_black.update(captured: false)
+    expected = false
+    actual = @rook_black.valid_move?(6, 7)
+    assert_equal expected, actual
+  end
 end
-
-
-
