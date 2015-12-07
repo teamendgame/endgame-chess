a510f6302ef123647ad3bf3a2f0d5b750ef9b0ee
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2015-12-07 01:51:34 +0000

    Add another guard statement and comment move_to

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,15 +2,18 @@ class Piece < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
 
-  # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/LineLength, Metrics/PerceivedComplexity, Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/LineLength, Metrics/MethodLength
   def move_to!(new_row, new_col)
     @piece = Piece.find_by(row_position: new_row, col_position: new_col)
-    if type == "Pawn" && check_adjacent_pieces(new_row, new_col)
-      capture_en_passant!(new_row, new_col, @last_updated) && return
-    end
+    # Checking for En Passant
+    capture_en_passant!(new_row, new_col, @last_updated) && return if type == "Pawn" && check_adjacent_pieces(new_row, new_col)
+    # Checking for Castling
     check_if_castling(new_row, new_col) if type == "King"
+    # Checking for Valid Move
     return unless valid_move?(new_row, new_col)
+    # If there is not a piece in the destination
     update(row_position: new_row, col_position: new_col, moved: true) && return unless @piece
+    # If there is a piece in the destination
     return unless @piece.user_id != user_id
     @piece.update(row_position: nil, col_position: nil, captured: true)
     update(row_position: new_row, col_position: new_col, moved: true)
