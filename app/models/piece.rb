f99615234530fefdad2252e36e33406101492c3f
Ronny Almog <ronny2205@gmail.com>
2015-12-10 18:32:43 +0000

    fixing errors

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -12,7 +12,7 @@ class Piece < ActiveRecord::Base
     # Checking for Valid Move
     return unless valid_move?(new_row, new_col)
     # Checking if the piece is moving into check
-    # return if moving_into_check?(new_row, new_col) != false
+    return if moving_into_check?(new_row, new_col)
     # If there is not a piece in the destination
     update(row_position: new_row, col_position: new_col, moved: true) && return unless @piece
     # Row & col were already updated in moving_into_check? method
