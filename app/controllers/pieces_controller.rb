d4a3f06a97110e5cfead02c6defa1b4fcfd07129
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2015-12-17 02:15:41 +0000

    Add flash alert if use tries an invalid move, add tests

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,5 +1,5 @@
 class PiecesController < ApplicationController
-  before_action :check_player_color
+  before_action :check_player_color, only: [:update]
 
   # rubocop:disable Metrics/AbcSize
   def show
@@ -10,8 +10,11 @@ class PiecesController < ApplicationController
   def update
     @piece = Piece.find(params[:id])
     @game = Game.find(@piece.game_id)
-    @piece.move_to!(piece_params[:row_position].to_i, piece_params[:col_position].to_i)
-    flash[:alert] = "Sorry, that's not a valid move for a #{@piece.type}" unless @piece.valid_move?(piece_params[:row_position].to_i, piece_params[:col_position].to_i)
+    row, col = piece_params[:row_position].to_i, piece_params[:col_position].to_i
+    unless @piece.valid_move?(row, col)
+      return redirect_to game_path(@game), status: 303, alert: "Sorry, that's not a valid move for a #{@piece.type}" 
+    end
+    @piece.move_to!(row, col)    
     @game.update_attributes(turn_number: @game.turn_number + 1)
     render text: 'updated!'
   end
