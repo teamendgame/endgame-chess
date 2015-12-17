f075660088da303d71ba5e300145bdb1e58a8751
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2015-12-17 02:24:47 +0000

    Auto-reload page after ajax put request

---
+++ app/assets/javascripts/games.js
@@ -0,0 +1,26 @@
+$(function() {
+  $('.move_mode').draggable({ containment: '.board'});
+  $( '.piece' ).droppable({
+    drop: function( event, ui ) {
+      $(ui.draggable).detach().css({top: 0,left: 0}).appendTo(this);
+      var draggableId = ui.draggable.attr("id");
+      var droppableId = $(this).attr("pos");
+      var updateUrl = "/pieces/" + draggableId
+      var row = droppableId[0];
+      var col = droppableId[1];
+        
+      $.ajax({
+        type: 'PUT',
+        url: updateUrl,
+        dataType: 'json',
+        data: { piece: { 
+                row_position: row, 
+                col_position: col 
+        }}, 
+        complete: function(){
+          location.reload(true);
+        }
+      });
+    }     
+  });  
+});
