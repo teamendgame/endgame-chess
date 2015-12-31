655a50dc9df99975e5f25f7ba5041a2f00cfa9a3
Jonathan Pike <jonathan.d.s.pike@gmail.com>
2015-12-31 09:15:15 +0000

    Add styling to mini chessboards, add booyah box around content

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  def which_king?(game)
+    return '<span class="king">&#9818;</span>'.html_safe if game.black_player_id == current_user.id
+    '<span class="king">&#9812;</span>'.html_safe if game.white_player_id == current_user.id
+  end
 end
