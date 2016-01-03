d6338dab575084b11b84013947c467c61522fe94
Ronny Almog <ronny2205@gmail.com>
2016-01-03 20:33:57 +0000

    adding a tooltip over king

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,9 @@ module UsersHelper
     return '<span class="king">&#9818;</span>'.html_safe if game.black_player_id == current_user.id
     '<span class="king">&#9812;</span>'.html_safe if game.white_player_id == current_user.id
   end
+
+  def which_color?(game)
+    return 'title="You are playing black"'.html_safe if game.black_player_id == current_user.id
+    'title="You are playing white"'.html_safe if game.white_player_id == current_user.id
+  end
 end
