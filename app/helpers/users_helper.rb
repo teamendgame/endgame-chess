dd43d40eb69d266128ee2f85f43c1a96b531678c
Ronny Almog <ronny2205@gmail.com>
2016-01-04 19:08:27 +0000

    applying bootstrap tooltips

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,6 @@
 module UsersHelper
   def which_king?(game)
-    return '<span class="king">&#9818;</span>'.html_safe if game.black_player_id == current_user.id
-    '<span class="king">&#9812;</span>'.html_safe if game.white_player_id == current_user.id
-  end
-
-  def which_color?(game)
-    return 'title="You are playing black"'.html_safe if game.black_player_id == current_user.id
-    'title="You are playing white"'.html_safe if game.white_player_id == current_user.id
+    return '<span class="king" rel="tooltip" data-placement="right" title="You are playing black" >&#9818;</span>'.html_safe if game.black_player_id == current_user.id
+    '<span class="king" rel="tooltip" data-placement="right" title="You are playing white">&#9812;</span>'.html_safe if game.white_player_id == current_user.id
   end
 end
